19a58af25d549cb33c31a1865f42b333f496254a
FehRoque <feliperamosroque@gmail.com>
2024-02-26 11:16:24 -0300

    BlogPost model

---
+++ src/models/BlogPost/index.ts
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+interface Post {
+  _id: Schema.Types.ObjectId;
+  title: string;
+  content: string;
+  category?: string;
+  tags: string[];
+  created_at: Date;
+  updated_at: Date;
+}
+
+const postSchema = new Schema<Post>({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  category: { type: String },
+  tags: { type: [String] },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+});
+
+export default model<Post>("Post", postSchema);
